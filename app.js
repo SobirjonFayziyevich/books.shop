bbd90b46b563648ec9e0cfd055be6f651c387c30
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2024-01-16 01:06:54 +0900

    BRR: Initial Setup

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Starting Web Server");
+const express = require("express");
+const app = express();
+const router = require("./router");
+const router_bssr = require("./router_bssr");
+
+// 1: Entry codes
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: Session codes
+
+// 3: Views codes
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing codes
+app.use("/book", router_bssr); // for admin and book users (BSSR)
+app.use("/", router); // for clients (SPA)
+
+module.exports = app;

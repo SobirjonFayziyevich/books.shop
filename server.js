39f02dad0b00d4b1ee7e259d5395a56b2a9495cb
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2024-01-16 01:11:56 +0900

    create project

---
+++ server.js
@@ -0,0 +1,29 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const http = require("http");
+const mongoose = require("mongoose");
+
+const connectionString = process.env.MONGO_URL;
+mongoose.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, goose) => {
+    if (err) console.log("Error on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      console.log("goose >>>", goose);
+
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = process.env.PORT || 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `This server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);

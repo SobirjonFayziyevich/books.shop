bb8c46a1a6ef2305be67674de5f86e599bc1a7d1
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2024-01-21 14:38:12 +0900

    fix: modify Signup link adminka

--- controllers/bookshopController.js
+++ controllers/bookshopController.js
@@ -136,14 +136,14 @@ bookshopController.logout = (req, res) => {
     }
 };
 
-bookshopController.validateAuthbookshop = (req, res, next) => {
+bookshopController.validateAuthbookshop = (req, res, next) => { 
     if (req.session?.member?.mb_type === "BOOKSHOP") {
         req.member = req.session.member;
         next();
     } else res.json({state: "fail", message: "only authenticated members with bookshop type"})
 };
 
-bookshopController.validateAdmin = (req, res, next) => {
+bookshopController.validateAdmin = (req, res, next) => { // faqat ADMIN user 
     if (req.session?.member?.mb_type === "ADMIN") {
         req.member = req.session.member;
         next();
@@ -167,6 +167,7 @@ bookshopController.checkSessions = (req, res) => {
 bookshopController.getAllBookshop = async (req, res) => {
     try {
         console.log("GET cont/getAllBookshop");
+        // TODO: call AllBookshop from DataBase:
         const book = new Book();
         const book_data = await book.getAllBookshopData();
         res.render("all-book", {book_data: book_data});
--- router.js
+++ router.js
@@ -4,12 +4,9 @@ const memberController = require("./controllers/memberController");
 const productController = require("./controllers/productController");
 const { getChosenMember } = require("./controllers/memberController");
 const { getAllProducts } = require("./controllers/productController");
-// const orderController = require("./controllers/orderController");
 const communityController = require("./controllers/communityController");
-// const followController = require("./controllers/followController");
 const bookshopController = require("./controllers/bookshopController");
 const uploader_community = require("./utils/upload-multer")("community"); //community adressi.
-// community argumenti asosida uploader objectini yasab beradi,
 const uploader_member = require("./utils/upload-multer")("members"); //members adressi.
 // member argumenti asosida uploader objectini yasab beradi,
 
@@ -22,69 +19,11 @@ router.post("/signup", memberController.signup);
 router.post("/login", memberController.login);
 router.get("/logout", memberController.logout);
 
-// Otherss routerlar
-router.get(
-  "/member/:id",
-  memberController.retrieveAuthMember,
-  memberController.getChosenMember
-);
+router.get("/menu", (req, res) => {
+  res.send("Menu Page");
+});
+router.get("/community", (req, res) => {
+  res.send("Community Page");
+});
 
-router.post(
-  "/member-liken",
-  memberController.retrieveAuthMember,
-  memberController.likeMemberChosen
-);
-
-router.post(
-  "/member/update",
-  memberController.retrieveAuthMember,
-  uploader_member.single("mb_image"),
-  memberController.updateMember
-);
-
-// BOOKSHOP RELATED ROUTERS
-router.get(
-  "/book",
-  memberController.retrieveAuthMember,
-  bookshopController.getBookshop
-);
-
-router.get(
-  "/book/:id",
-  memberController.retrieveAuthMember,
-  bookshopController.getChosenBookshop
-);
-
-router.post(
-    "/community/image",
-    uploader_community.single("community_image"), //single mathod orqali imageni community_image nomi bn backendga yubordim.
-    communityController.imageInsertion
-  ); //keyingi mantiqim communityControllerni hosil qilib unga,maxsus imageInsertion degan mathodni yozib oldim.
-  
-  router.post(
-    "/community/create",
-    memberController.retrieveAuthMember,
-    communityController.createArticle
-  );
-  
-  router.get(
-    "/community/articles",
-    memberController.retrieveAuthMember,
-    communityController.getMemberArticles
-  );
-  
-  router.get(
-    "/community/target",
-    memberController.retrieveAuthMember, // buyerda harqanday atriclega like bosganmizmi, yuqmi shuni topish un uzimizni retrive qilishimz kerak.
-    communityController.getArticles
-  );
-  
-  router.get(
-    "/community/single-article/:art_id",
-    memberController.retrieveAuthMember, // buyerda harqanday atriclega like bosganmizmi, yuqmi shuni topish un uzimizni retrive qilishimz kerak.
-    communityController.getChosenArticle
-  );
-  
-
-// export router
 module.exports = router;
